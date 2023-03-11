7149ed9bc5dfd45bf1fe99fa38e66d92e2fb17c4
Albert Zhan <albert.cfzhan@gmail.com>
2023-03-10 23:07:30 -0800

    Created object pooling scripts
    haven't implemented them yet though

---
+++ Assets/Scripts/Object Pooling/ObjectPooler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/*
+ * 
+ */
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler sharedInstance;
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    [SerializeField] private int amountToPool;
+
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Loop through list of pooled objects,deactivating them and adding them to the list 
+        pooledObjects = new List<GameObject>();
+        for (var i = 0; i < amountToPool; i++)
+        {
+            var obj = Instantiate(objectToPool, transform, true);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        // For as many objects as are in the pooledObjects list, if the pooled objects is NOT active, return that object, otherwise return null
+        return pooledObjects.FirstOrDefault(t => !t.activeInHierarchy);
+    }
+}
