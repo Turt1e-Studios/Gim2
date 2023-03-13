c2e35a33c62f83fa315aa8b1f08bcdffe06845c3
Albert Zhan <albert.cfzhan@gmail.com>
2023-03-12 22:10:12 -0700

    Added spawn rate change
    spawn rate gets faster throughout the level

--- Assets/Scripts/Enemies/SpawnEnemy.cs
+++ Assets/Scripts/Enemies/SpawnEnemy.cs
@@ -7,12 +7,17 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour
 {
     [SerializeField] private List<GameObject> enemies;
-    [SerializeField] private float spawnRate = 3.0f;
-    private const int Range = 10;
+    [SerializeField] private float initialSpawnRate = 3.0f;
+    [SerializeField] private float spawnRateChange = 0.05f;
+    [SerializeField] private int range = 10;
+    
+    private const float MinimumSpawnRate = 0.5f;
+    private float spawnRate;
 
     // Start is called before the first frame update
     private void Start()
     {
+        spawnRate = initialSpawnRate;
         StartCoroutine(EnemySpawn());
     }
 
@@ -21,9 +26,16 @@ public class SpawnEnemy : MonoBehaviour
         while(true)
         {
             yield return new WaitForSeconds(spawnRate);
+            
             var position = transform.position;
-            Vector3 newPosition = new Vector3(position.x + Random.Range(-Range, Range), position.y, position.z + Random.Range(-Range, Range));
+            Vector3 newPosition = new Vector3(position.x + Random.Range(-range, range), position.y, position.z + Random.Range(-range, range));
             Instantiate(enemies[Random.Range(0, enemies.Count)], newPosition, transform.rotation);
+            
+            spawnRate -= spawnRateChange;
+            if (spawnRate < MinimumSpawnRate)
+            {
+                spawnRate = MinimumSpawnRate;
+            }
         }
     }
 }
