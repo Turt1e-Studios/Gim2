819d79b8c6e018bb95b476064b2224d9c533eb32
Albert Zhan <albert.cfzhan@gmail.com>
2023-04-06 21:51:13 -0700

    Bullet physics
    also added sfx files
    probably should have seperated this into multiple commits

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 // Just bullet that damages the player I thinK?
@@ -8,9 +9,25 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.gameObject.CompareTag("enemy")) return;
-        
-        other.GetComponent<Enemy>().ChangeHealth(-damage);
-        Destroy(gameObject);
+        // Enemy loses health when colliding with the bullet
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            other.GetComponent<Enemy>().ChangeHealth(-damage);
+        }
+
+        // Destroy the bullet if it collides with something
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        // Probably could also implement this for x and z directions too. Not sure how to do this while still being performant
+        if (transform.position.y < -Settings.outOfBoundsBox)
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    #region Inspector Variables
+    #endregion
+    #region Public Variables
+
+    public static float outOfBoundsBox = 100f;
+
+    #endregion
+    #region Private Variables
+    #endregion
+    #region Protected Variables
+    #endregion
+    
+    #region Unity Methods
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+    #region Protected Methods
+    #endregion
+}
