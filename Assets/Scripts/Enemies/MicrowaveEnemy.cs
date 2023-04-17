204eebeef23f1d6432cbaf5d009a2cb7cb932cbc
Albert Zhan <albert.cfzhan@gmail.com>
2023-04-16 17:54:33 -0700

    Microwave enemy actually points towards you

--- Assets/Scripts/Enemies/MicrowaveEnemy.cs
+++ Assets/Scripts/Enemies/MicrowaveEnemy.cs
@@ -8,6 +8,7 @@ public class MicrowaveEnemy : Enemy
     [SerializeField] private float maxHeight = 10f;
     [SerializeField] private float secondsPerShot = 3f;
     [SerializeField] private float bulletSpeed = 20f;
+    [SerializeField] private float damping = 1f;
 
     private GameObject player;
     private Transform _transform;
@@ -24,23 +25,30 @@ public class MicrowaveEnemy : Enemy
     // Update is called once per frame
     private void Update()
     {
+        // Makes the microwave rise into the air
         if (transform.position.y <= maxHeight)
         {
             transform.Translate(Vector3.up * (speed * Time.deltaTime));
         }
+        // Shoot stuff
         else if (!isShooting)
         {
             isShooting = true;
             InvokeRepeating(nameof(Shoot), 0f, secondsPerShot);
         }
-        transform.LookAt(player.transform);
+        
+        var lookPos = player.transform.position - transform.position;
+        lookPos.y = 0;
+        lookPos.z = 0; // remove this line if you don't like how it rotates
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
     }
 
     private void Shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab, _transform.position, _transform.rotation);
         bullet.GetComponent<Bullet>().Source = gameObject;
-        bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed; 
+        bullet.GetComponent<Rigidbody>().velocity = (player.transform.position - transform.position) * bulletSpeed; 
     }
 }
 
